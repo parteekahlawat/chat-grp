11ef40fb3d63ed0ba10b25cabfe6f12667608921
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2024-04-03 13:17:34 +0530

    vercel added

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const { Server } = require("socket.io");
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 
-const app = next({ dev });
+const hostname = "localhost";
+const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
